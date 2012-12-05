3ac10617864c15e7d8934a6a48965b164a4901fc
cjsg1 <cjsg1@cms-gb04-20.cms.waikato.ac.nz>
2012-12-05 14:56:49 +1300

    Hopefully the Avatar is pushed

---
+++ NotFussed/NotFussed/cameraController.h
@@ -0,0 +1,13 @@
+//
+//  cameraController.h
+//  NotFussed
+//
+//  Created by cjsg1 on 5/12/12.
+//  Copyright (c) 2012 The University of Waikato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cameraController : UIViewController
+
+@end
---
+++ NotFussed/NotFussed/cameraController.m
@@ -0,0 +1,38 @@
+//
+//  cameraController.m
+//  NotFussed
+//
+//  Created by cjsg1 on 5/12/12.
+//  Copyright (c) 2012 The University of Waikato. All rights reserved.
+//
+
+#import "cameraController.h"
+
+@interface cameraController ()
+
+@end
+
+@implementation cameraController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

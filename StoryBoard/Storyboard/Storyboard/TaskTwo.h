b1df70ae807fc9aff57df6dc9db6f685162b58e0
KiwiAzn <ceilingazn@gmail.com>
2012-12-13 09:54:52 +1300

    Added the Trait selection task.

--- StoryBoard/Storyboard/Storyboard/TaskTwo.h
+++ StoryBoard/Storyboard/Storyboard/TaskTwo.h
@@ -8,6 +8,15 @@
 
 #import "TaskController.h"
 
-@interface TaskTwo : TaskController
-
+@interface TaskTwo : TaskController {
+BOOL btnStates[11];
+int maxSelectedTraits;
+}
+@property (nonatomic,retain) IBOutletCollection(UIButton) NSArray *traits;
+@property (nonatomic,strong) IBOutlet UILabel *traitDisplay;
+@property (nonatomic,strong) IBOutlet UITextField *tfCustomTrait;
+@property (nonatomic,strong) UIColor *selectedColour;
+@property (nonatomic,strong) UIColor *orginalColour;
+-(IBAction)btnTraitPressed:(id)sender;
+-(IBAction)tfTraitEntered:(id)sender;
 @end
